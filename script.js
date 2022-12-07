c9e390761af568992c75d8a039abcaf4817f49b0
KaiqiGeorge <oliviakaiqihuang@gmail.com>
2022-12-07 14:49:54 -0700

    add the backspace

--- script.js
+++ script.js
@@ -10,15 +10,27 @@ btns.map((button) => {
         break;
       case "←":
         if (display.innerText) {
-          display.innerText = Array.from(display.innerText).slice(0,-1);
+          display.innerText = Array.from(display.innerText).slice(0, -1);
+        }
+        break;
+      case "+/-":
+        if (!display.innerText) {
+        } else if (display.innerText > 0) {
+          display.innerText = -Math.abs(display.innerText);
+        } else {
+          display.innerText = Math.abs(display.innerText);
         }
         break;
       case "=":
-        try {
-          display.innerText = Math.round(eval(display.innerText) * 100) / 100;
-        } catch {
-          display.innerText = "error";
+        if (!display.innerText) {
+        } else {
+          try {
+            display.innerText = Math.round(eval(display.innerText) * 100) / 100;
+          } catch {
+            display.innerText = "error";
+          }
         }
+
         break;
       default:
         display.innerText += e.target.innerText;
